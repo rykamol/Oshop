340b737812dbc03ed2fea97a3302f7dfc033fa1b
Kamol Roy <kamol.roy9943@gmail.com>
2018-11-26 22:01:08 +0600

    initial commit with firebase configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCAiuPf6aekbkwKKInjc8LJzTMTl4TXpU0",
+    authDomain: "oshop-8412b.firebaseapp.com",
+    databaseURL: "https://oshop-8412b.firebaseio.com",
+    projectId: "oshop-8412b",
+    storageBucket: "",
+    messagingSenderId: "826988555499"
+  }
+};
